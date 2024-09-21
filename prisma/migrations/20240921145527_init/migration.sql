0bffb4ad9dfb46f954f358f21f75f513d71dd28e
harx-dev <harshathmkulal555@gmail.com>
2024-09-21 20:32:14 +0530

    Added nextauth

--- prisma/migrations/20240921145527_init/migration.sql
+++ prisma/migrations/20240921145527_init/migration.sql
@@ -4,6 +4,8 @@ CREATE TABLE "User" (
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
